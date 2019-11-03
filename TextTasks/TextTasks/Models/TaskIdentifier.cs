abefa5e958b5c0a4b9369d6d5b4384da3c032b45
Florin-Catalin <florin.g.ungureanu@gmail.com>
2019-11-03 20:41:44 +0200

    second commit

---
+++ TextTasks/TextTasks/Models/TaskIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextTasks.Models
+{
+    public class TaskIdentifier
+    {
+        public int TaskId { get; set; }
+    }
+}
